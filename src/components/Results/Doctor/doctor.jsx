39a3cc69c969032c0c8cbcbca200ac098a6f930f
guigail <guillaume.crespel@gmail.com>
2016-12-11 23:03:29 +0100

    Modification mineur style

--- src/components/Results/Doctor/doctor.jsx
+++ src/components/Results/Doctor/doctor.jsx
@@ -1,32 +1,36 @@
-import React, { PropTypes } from 'react'
+import React, {PropTypes} from 'react'
 import loader from 'hoc-react-loader'
-import { ListItem, FontIcon, Tooltip, IconButton } from 'react-toolbox'
+import {ListItem, FontIcon, Tooltip, IconButton} from 'react-toolbox'
+import {styles} from './doctor.style'
+
 const TooltipButton = Tooltip(IconButton)
-const Doctor = ({ id, doctor, haveAppointment, showOnMap, showInfo }) => (
-  <ListItem
-    selectable
-    onClick={() => {showOnMap(id, true); showInfo(id)}}
-    avatar='https://dl.dropboxusercontent.com/u/2247264/assets/o.jpg'
-    caption={doctor.name}
-    legend={doctor.address.address_street}
-    onMouseUp={() => alert("COUOCU")}
-    rightIcon={!haveAppointment ?
-      <TooltipButton label=''
-                     icon='date_range'
-                     primary
-                     tooltip='Prendre rendez-vous'
-                     tooltipDelay={500}
-                     tooltipPosition='right'/>
-                     : 'date_range'
-    }
-  />
+const Doctor = ({id, doctor, haveAppointment, showOnMap, showInfo}) => (
+    <ListItem
+        themes={styles}
+        selectable
+        caption={doctor.name}
+        legend={doctor.address.address_street}
+        rightIcon={!haveAppointment ?
+            <TooltipButton label=''
+                           icon='date_range'
+                           primary
+                           tooltip='Prendre rendez-vous'
+                           tooltipDelay={500}
+                           tooltipPosition='right'/>
+            : 'date_range'
+        }
+        onClick={() => {
+            showOnMap(id, true);
+            showInfo(id)
+        }}
+    />
 )
 
 Doctor.propTypes = {
-  className: PropTypes.string,
-  id: PropTypes.number.isRequired,
-  doctor: PropTypes.object.isRequired,
-  lastOne: PropTypes.bool,
+    className: PropTypes.string,
+    id: PropTypes.number.isRequired,
+    doctor: PropTypes.object.isRequired,
+    lastOne: PropTypes.bool,
 }
 
-export default loader(Doctor, { wait: false })
+export default loader(Doctor, {wait: false})
